41159de2ed8120dbf794785f94758c90f780fa8e
Af0ns0836 <dias4018@gmail.com>
2023-11-06 00:02:35 +0000

    free move feito, falta drop move

--- src/main.pl
+++ src/main.pl
@@ -1,18 +1,128 @@
 :- consult('board.pl').
+:- consult('endgame.pl').
 :- use_module(library(lists)).
 
+% predicate that returns the piece in a certain position, first selects the row, and then the piece itself
+% arguments, Board, the row and the column and the piece will be returned in the piece variable
+get_piece(Board, Row, Column, Piece) :-
+    element_at(Row, Board, RowList),         
+    element_at(Column, RowList,Piece).
 
-% Makes a move on the board.
-make_move(Board, Row, Col, Piece, NewBoard) :-
+
+% Predicate to make a move based on the move type.
+make_move(Board, Row, Col, Piece, NewBoard, 'free') :-
+    write('free move'),nl,
+    get_valid_move(Board, Row, Col),
+    free_move(Board, Piece, [Row, Col], NewBoard).
+
+make_move(Board, Row, Col, 'X', NewBoard, 'drop') :-
+    write('drop move'), nl,
+    write('Piece: X'), nl,
+    write('Choose the opponents piece you want to drop on:'),nl,
+    correct_drop_move(Board, Row, Col, 'X'),
+    drop_move(Board, 'X', 'O', [Row, Col], NewBoard).
+
+make_move(Board, Row, Col, 'O', NewBoard, 'drop') :-
+    write('drop move'), nl,
+    write('Piece: O'), nl,
+    correct_drop_move(Board, Row, Col, 'O',OpponentPieceCol,OpponentPieceRow),
+    drop_move(Board, 'O', 'X', [OpponentPieceRow, OpponentPieceCol], NewBoard).
+
+correct_drop_move(Board, Row, Col, 'O',OpponentPieceCol,OpponentPieceRow) :-
+    repeat,
+    write('Choose the piece you want to drop on:'), nl,
+    write('Row:'), read(OpponentPieceRow),
+    write('Col:'), read(OpponentPieceCol),
     (
-        is_valid_move(Board, Row, Col),
-        get_row(Board, Row, OldRow),
-        replace(OldRow, Col, Piece, NewRow),
-        replace_row(Board, Row, NewRow, NewBoard)
-    ;
-        write('Invalid move. Please try again.'), nl,
-        NewBoard = Board
+        integer(OpponentPieceRow), integer(OpponentPieceCol),
+        OpponentPieceRow >= 0, OpponentPieceRow < 8,
+        OpponentPieceCol >= 0, OpponentPieceCol < 8,
+        get_piece(Board, OpponentPieceRow, OpponentPieceCol, Piece),
+        write('Piece found: '), write(Piece), nl,
+        Piece == 'X' -> write('Valid move.'), nl,
+        true; % Valid move, exit the loop
+        write('Invalid move. Please use valid row and column inputs.'), nl,
+        fail % Retry the loop
     ).
+
+correct_drop_move(Board, Row, Col, 'X',OpponentPieceCol,OpponentPieceRow) :-
+    repeat,
+    write('Choose the piece you want to drop on:'), nl,
+    write('Row:'), read(OpponentPieceRow),
+    write('Col:'), read(OpponentPieceCol),
+    (
+        integer(OpponentPieceRow), integer(OpponentPieceCol),
+        OpponentPieceRow >= 0, OpponentPieceRow < 8,
+        OpponentPieceCol >= 0, OpponentPieceCol < 8,
+        get_piece(Board, OpponentPieceRow, OpponentPieceCol, Piece),
+        write('Piece found: '), write(Piece), nl,
+        Piece == 'O' -> write('Valid move.'), nl,
+        true; % Valid move, exit the loop
+        write('Invalid move. Please use valid row and column inputs.'), nl,
+        fail % Retry the loop
+    ).
+
+
+% Predicate to replace an element in a matrix.
+replace_in_matrix([H|T], 0, Col, X, [R|T]) :-
+    replace(H, Col, X, R).
+replace_in_matrix([H|T], Row, Col, X, [H|R]) :-
+    Row > 0,
+    Row1 is Row - 1,
+    replace_in_matrix(T, Row1, Col, X, R).
+
+% Predicate to find a free adjacent square.
+find_free_adjacent(Board, Row, Col, [NewRow, NewCol]) :-
+    member([DeltaRow, DeltaCol], [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]),
+    NewRow is Row + DeltaRow,
+    NewCol is Col + DeltaCol,
+    is_valid_move(Board, NewRow, NewCol).
+
+
+% Predicate for a drop move.
+drop_move(Board, PlayerPiece, OpponentPiece, [OpponentPieceRow, OpponentPieceCol], NewBoard) :-
+    write('drop_move'), nl,
+    display_board(Board),
+    write('Player piece '), write(PlayerPiece), write(' found at '), write(OpponentPieceRow), write(', '), write(OpponentPieceCol), nl,
+    write('Opponent piece '), write(OpponentPiece), write(' found at '), write(OpponentPieceRow), write(', '), write(OpponentPieceCol), nl,
+    write('Where do you want to place it? (left, right, up, down, up-right, up-left, down-left, down-right): '), nl,
+    read(Direction),
+    
+    % Get the direction offsets based on the chosen direction.
+    direction_offset(Direction, DeltaX, DeltaY),
+
+    % Calculate the new position.
+    NewCol is OpponentPieceCol + DeltaX,
+    NewRow is OpponentPieceRow + DeltaY,
+
+    write('New position: '), write(NewRow), write(', '), write(NewCol), nl,
+    % Ensure the new position is a valid move.
+    %is_valid_move(Board, NewRow, NewCol) -> write('Valid move.'), nl,
+    % Replace the OpponentPieceRow and OpponentPieceCol with the player's piece in NewBoard.
+    display_board(Board),
+    replace_in_matrix(Board, OpponentPieceRow, OpponentPieceCol, PlayerPiece, NewBoard),
+    replace_in_matrix(Board, NewRow, NewCol, OpponentPiece, NewBoard),
+    display_board(NewBoard).
+    % Replace the target position with the opponent's piece in NewBoard.
+    %replace_in_matrix(Board, NewRow, NewCol, PlayerPiece, NewBoard).
+
+
+ % Define the direction offsets.
+direction_offset(left, DeltaX, DeltaY) :- DeltaX is -1, DeltaY is 0.
+direction_offset(right, DeltaX, DeltaY) :- DeltaX is 1, DeltaY is 0.
+direction_offset(up, DeltaX, DeltaY) :- DeltaX is 0, DeltaY is -1.
+direction_offset(down, DeltaX, DeltaY) :- DeltaX is 0, DeltaY is 1.
+direction_offset(up-right, DeltaX, DeltaY) :- DeltaX is 1, DeltaY is -1.
+direction_offset(up-left, DeltaX, DeltaY) :- DeltaX is -1, DeltaY is -1.
+direction_offset(down-left, DeltaX, DeltaY) :- DeltaX is -1, DeltaY is 1.
+direction_offset(down-right, DeltaX, DeltaY) :- DeltaX is 1, DeltaY is 1.
+
+
+% Predicate for a free move.
+free_move(Board, PlayerPiece, [Row, Col], NewBoard) :-
+    is_valid_move(Board, Row, Col),
+    replace_in_matrix(Board, Row, Col, PlayerPiece, NewBoard).
+
 
 % Predicate to replace an element in a list.
 replace([_|T], 0, X, [X|T]).
@@ -26,22 +136,33 @@ is_valid_move(Board, Row, Col) :-
     length(Board, NumRows),
     length(Board, NumCols),
     Row >= 0, Row < NumRows,
-    Col >= 0, Col < NumCols.
-    %free_move_possible(Board, Row, Col).
+    Col >= 0, Col < NumCols,
+    get_row(Board, Row, RowList), % Get the row
+    nth0(Col, RowList, 'empty'),
+    \+has_adjacent_pieces(Board, Row, Col).
 
-free_move_possible(Board) :-
-    (member(Row, Board), member(empty, Row)).
+% Predicate to check if there are adjacent pieces to a given position.
+has_adjacent_pieces(Board, Row, Col) :-
+    adjacent_positions(Row, Col, Adjacent),
+    member([X, Y], Adjacent),
+    get_row(Board, X, XRow),
+    nth0(Y, XRow, Piece),
+    Piece \== 'empty'.
 
-% Predicate to check if a piece is adjacent to another piece diagonally and ortoganally.
-adjacent([X,Y], [X1,Y1]) :-
-    (X1 is X+1, Y1 is Y);  % right
-    (X1 is X-1, Y1 is Y);  % left
-    (X1 is X, Y1 is Y+1);  % up
-    (X1 is X, Y1 is Y-1);  % down
-    (X1 is X+1, Y1 is Y-1);  % diagonal up right
-    (X1 is X-1, Y1 is Y+1);  % diagonal up left
-    (X1 is X+1, Y1 is Y+1);  % diagonal down right
-    (X1 is X-1, Y1 is Y-1).  % diagonal down left
+% Predicate to get positions adjacent to a given position.
+adjacent_positions(Row, Col, Adjacent) :-
+    RowPlus1 is Row + 1,
+    RowMinus1 is Row - 1,
+    ColPlus1 is Col + 1,
+    ColMinus1 is Col - 1,
+    Adjacent = [[RowMinus1, Col], [RowPlus1, Col], [Row, ColMinus1], [Row, ColPlus1],
+                [RowMinus1, ColMinus1], [RowPlus1, ColPlus1], [RowMinus1, ColPlus1], [RowPlus1, ColMinus1]],
+    include(is_valid_position, Adjacent, ValidAdjacent).
+
+% Predicate to check if a position is valid (within the board boundaries).
+is_valid_position([Row, Col]) :-
+    Row >= 0, Row < 8, Col >= 0, Col < 8.
+
 
 % Custom predicate to retrieve a specific row from a list of lists.
 get_row([Row | _], 0, Row).
@@ -57,25 +178,9 @@ replace_row([Row | Rest], Index, NewRow, [Row | UpdatedRest]) :-
     NextIndex is Index - 1,
     replace_row(Rest, NextIndex, NewRow, UpdatedRest).
 
-% This assumes the game alternates between player1 and player2.
-play(Player, Board, NewBoard) :-
-    display_board(Board),
-    write(Player), write('\'s turn.'), nl,
-    free_move(Board, Row, Col),
-    player_piece(Player, Piece),
-    make_move(Board, Row, Col, Piece, NewBoard).
-
-% Drop move predicate
-% drop_move(+Board, +).
-drop_move(Board, Row, Col):-
-    get_row(Board, Row, OldRow),
-    replace(OldRow, Col, 'X', NewRow),
-    replace_row(Board, Row, NewRow, NewBoard).
-
 % Predicate to get a valid move from the player . Free move
 % get_valid_move(+Board, -Row, -Col).
-free_move(Board, Row, Col) :-
-    %free_move_possible(Board),
+get_valid_move(Board, Row, Col) :-
     repeat,
     write('Enter the row (0-7): '),
     read(RowTerm),
@@ -87,11 +192,25 @@ free_move(Board, Row, Col) :-
         ColTerm >= 0, ColTerm < 8,
         Row is RowTerm,
         Col is ColTerm,
-        is_valid_move(Board, Row, Col) ->
+        is_valid_move(Board, Row, Col) -> write('Valid move.'), nl,
         true; % Valid move, exit the loop
         write('Invalid move. Please use valid row and column inputs.'), nl,
         fail % Retry the loop
     ).
+
+play(Player, Board, NewBoard, MoveType, NextMoveType) :-
+    display_board(Board),
+    write('Move type: '), write(MoveType), nl, 
+    write(Player), write('\'s turn.'), nl,
+    %get_valid_move(Board, Row, Col),
+    player_piece(Player, Piece),
+    make_move(Board, Row, Col, Piece, NewBoard, MoveType),
+    next_move_type(MoveType, NextMoveType).
+
+
+% Predicate to get the next move type.
+next_move_type('free', 'drop').
+next_move_type('drop', 'free').
 
 % Initalize the game.
 play_game :-
@@ -104,7 +223,7 @@ play_game :-
         Option == 1 -> write('Player vs Player selected.'), nl;
         Option == 2 -> write('Player vs Computer selected.'), nl
     ),
-    play_loop(player1, Board,Option).
+    play_loop(player1, Board, Option, 'free').
 
 % Predicates to loop through the game.
 play_loop(_, Board, _) :-
@@ -112,11 +231,11 @@ play_loop(_, Board, _) :-
     display_board(Board),
     write('Game over! Winner: '), write(Winner), nl.
 
-play_loop(Player, Board,Option) :-
+play_loop(Player, Board,Option, MoveType) :-
     \+ game_over(Board, _), % Check if the game is not over
-    play(Player, Board, NewBoard),
+    play(Player, Board, NewBoard, MoveType, NextMoveType),
     switch_player(Player, NextPlayer, Option),
-    play_loop(NextPlayer, NewBoard).
+    play_loop(NextPlayer, NewBoard,_,NextMoveType).
 
 % Predicate to switch players.
 switch_player(Player, NextPlayer, Option) :-
@@ -142,87 +261,3 @@ game_over(Board, Winner) :-
     write('White group count: '), write(WhiteGroups), nl,
     compare_winner(BlackGroups, WhiteGroups, Winner).
 
-% predicate to check the size of the group of pieces of a player
-check_group_size(Board, Player, SortedDescendingSizes) :-
-    write('checking group size...'), nl,
-    findall([X, Y], (member(X, [0,1,2,3,4,5,6,7]), member(Y, [0,1,2,3,4,5,6,7]), get_piece(Board, X, Y, Player)), List),
-    largest_connected_component(List, SortedDescendingSizes).
-
-element_at(0, [Elem|_], Elem).
-element_at(N, [_|Rest], Elem) :-
-    N > 0,
-    N1 is N - 1,
-    element_at(N1, Rest, Elem).
-
-% predicate that returns the piece in a certain position, first selects the row, and then the piece itself
-% arguments, Board, the row and the column and the piece will be returned in the piece variable
-get_piece(Board, Row, Column, Piece) :-
-    element_at(Row, Board, RowList),         
-    element_at(Column, RowList,Piece).
-
-:- dynamic visited/1.
-% Predicate to find the size of the largest connected component
-largest_connected_component(Points, SortedDescendingSizes) :-
-    findall(Size, (
-        member(Point, Points),
-        \+ visited(Point),  % Ensure we dont start BFS from visited points
-        bfs_from_point(Point, Points, [Point], Size),
-        asserta(visited(Point))  % Mark the starting point as visited after BFS
-    ), Sizes),
-    sort(Sizes, SortedSizes),
-    reverse(SortedSizes,SortedDescendingSizes),
-    retractall(visited(_)). % Clean up visited facts after computation
-
-% Predicate for BFS traversal from a given point
-bfs_from_point(Point, Points, Visited, Size) :-
-    bfs([Point], Points, Visited, 1, Size).
-
-% Helper predicate for BFS traversal
-bfs([], _, _, Size, Size). % Base case: no more points to traverse
-bfs([P|RestQueue], Points, Visited, CurrentSize, Size) :-
-    find_connected(P, Points, Visited, ConnectedPoints),
-    length(ConnectedPoints, NumConnected),
-    NewSize is CurrentSize + NumConnected,
-    append(RestQueue, ConnectedPoints, NewQueue),
-    append(Visited, ConnectedPoints, NewVisited),
-    bfs(NewQueue, Points, NewVisited, NewSize, Size).
-
-% Predicate to find all points connected to a given point that havent been visited
-find_connected(Point, Points, Visited, ConnectedPoints) :-
-    include(is_connected(Point, Visited), Points, ConnectedPoints).
-
-% Predicate to check if a point is connected and not visited
-is_connected([X,Y], Visited, [X1,Y1]) :-
-    adjacent([X,Y], [X1,Y1]),
-    \+ memberchk([X1,Y1], Visited).
-
-% Predicate to check if two points are adjacent and potentially connected
-adjacent([X,Y], [X1,Y1]) :-
-    (X1 is X+1, Y1 is Y);  % right
-    (X1 is X-1, Y1 is Y);  % left
-    (X1 is X, Y1 is Y+1);  % up
-    (X1 is X, Y1 is Y-1).  % down
-
-
-% Remove all instances of elements in the second list from the first list
-remove_elements(List, [], List).
-remove_elements(List, [H|T], Result) :-
-    delete(List, H, TempResult), % delete/3 is a built-in predicate
-    remove_elements(TempResult,T,Result).
-
-% compare_winner(+GroupSize1, +GroupSize2, -Winner).
-% Predicate to compare group sizes and determine the winner.
-compare_winner(BlackGroups, WhiteGroups, Winner) :-
-    write('Comparing group sizes...'), nl,
-    compare_lists(BlackGroups, WhiteGroups, Winner).
-
-
-% Compare two lists element by element
-compare_lists([], [], Winner) :-
-   Winner = 'Draw'. % Both lists are equal.
-
-% compare_list(+List1, +List2, -Winner).
-compare_lists([X|Xs], [Y|Ys], Winner) :-
-    (write('Comparing '),write(X), write('>'),write(Y),nl ,X > Y -> Winner = 'Black';  % Check if X is greater than Y
-    X < Y -> Winner = 'White';  % Check if Y is greater than X
-    compare_lists(Xs, Ys, Winner)).  % Recurse to compare the rest of the lists
